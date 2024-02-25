fa939470ffa9d1ac5af69fc6c5f527007aed3f7c
Laura <70455158+LauraKapitza@users.noreply.github.com>
2024-02-25 02:12:25 +0100

    Initial commit

---
+++ src/main/java/com/featurevotes/featurevotes/FeatureVotesApplication.java
@@ -0,0 +1,13 @@
+package com.featurevotes.featurevotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureVotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureVotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/featurevotes/featurevotes/FeatureVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featurevotes.featurevotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureVotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
