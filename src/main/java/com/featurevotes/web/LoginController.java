23930b696ff27da25d4fdb19c8dae3f8e5114d9f
Laura <laura.kapitza@gmail.com>
2024-04-04 22:25:56 +0200

    create registration form

--- src/main/java/com/featurevotes/web/LoginController.java
+++ src/main/java/com/featurevotes/web/LoginController.java
@@ -1,7 +1,11 @@
 package com.featurevotes.web;
 
+import com.featurevotes.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -11,7 +15,14 @@ public class LoginController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(ModelMap model) {
+        model.put("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String createAccount(User user) {
+        System.out.println(user.toString());
+        return "redirect:/login";
     }
 }
