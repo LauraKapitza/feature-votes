765818eed0a31c4cc332389265a797415f6b7036
Laura <laura.kapitza@gmail.com>
2024-05-17 23:21:48 +0200

    retrieve product from db

--- src/main/java/com/featurevotes/web/ProductController.java
+++ src/main/java/com/featurevotes/web/ProductController.java
@@ -3,13 +3,18 @@ package com.featurevotes.web;
 import com.featurevotes.domain.Product;
 import com.featurevotes.domain.User;
 import com.featurevotes.repository.ProductRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -23,7 +28,15 @@ public class ProductController {
     }
 
     @GetMapping("/products/{productId}")
-    public String getProduct (@PathVariable int productId) {
+    public String getProduct (@PathVariable int productId, ModelMap model, HttpServletResponse response) throws IOException {
+        Optional<Product> productOpt = productRepository.findById(productId);
+
+        if(productOpt.isPresent()) {
+            Product product = productOpt.get();
+            model.put("product", product);
+        } else {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " was not found");
+        }
         return "product";
     }
 
